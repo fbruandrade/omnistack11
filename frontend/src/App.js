979c146ceeea6546abd0a16689cec949dd06944e
Felipe Brunelli de Andrade <fbruandrade@gmail.com>
2020-03-28 23:16:16 -0300

    Frontend da Aplicação

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'; // , { useState }
+
+import './global.css';
+
+import Routes from './routes';
+// import Header from './Header';
 
 function App() {
+  // const [counter, setCounter] = useState(0);
+
+  // Array [valor, funcao de atualizacao]
+  
+  // function increment() {
+  //   setCounter(counter + 1);
+  // }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello OmniStack 11.0
-          {/* Edit <code>src/App.js</code> and save to reload. */}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes />
+    // <div>
+    //   <Header>Contator: {counter}</Header>
+    //   <button onClick={increment}>Incrementar</button>
+    // </div>
   );
 }
 
 export default App;
+ 
